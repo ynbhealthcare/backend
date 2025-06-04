3dbed22e810e1ecc3f40ba4c7c3bb051e02c41df
ynbhealthcare <dev.ynbhealthcare@gmail.com>
2025-06-04 15:35:34 +0530

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+ import mongoose from "mongoose";
 import blogModel from "../models/blogModel.js";
 import userModel from "../models/userModel.js";
 import chatModel from "../models/chatModel.js";
